d0138ff335b661435a4048c3742d1608e502ce5c
prozario-ops <prozario@umd.edu>
2026-03-04 22:01:10 -0500

    added extract.py and updated gitignore/requirements

--- src/etl/extract.py
+++ src/etl/extract.py
@@ -1,2 +1,27 @@
 import pandas as pd
 import os
+from pathlib import  Path
+
+def extract():
+    project_root = Path(__file__).resolve().parent
+
+    raw_dir= project_root / 'data' / 'raw'
+
+    food= pd.read_csv(raw_dir / 'food.csv')
+    nutrient= pd.read_csv(raw_dir / 'nutrient.csv')
+    food_nutrient= pd.read_csv(raw_dir / 'food_nutrient.csv')
+    branded_food= pd.read_csv(raw_dir / 'branded_food.csv')
+
+    fmap= pd.read_csv(raw_dir / 'fmap.csv')
+
+    return {
+        'food': food,
+        'nutrient': nutrient,
+        'food_nutrient': food_nutrient,
+        'branded_food': branded_food,
+        'fmap': fmap
+    }
+
+
+
+
