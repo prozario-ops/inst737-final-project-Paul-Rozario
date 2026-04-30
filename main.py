54e433d4e409eb092ea452d97efbc9a15e6c44b7
prozario-ops <prozario@umd.edu>
2026-04-30 18:52:57 -0400

    remove commented-out print statements from main.py to clean up code

--- main.py
+++ main.py
@@ -13,62 +13,6 @@ logging.basicConfig(
 )
 
 def main():
-#     data = extract()
-#     print("running data extraction...\n")
-#     if data:
-#         print("Data extraction successful.\n")
-
-#         for key, df in data.items():
-#             print(f"{key} shape: {df.shape}")
-
-#     else:
-#         print("Data extraction failed.")
-#         return
-
-
-#     cereal_df, fmap_df = transform(data)
-
-#     if cereal_df is not None and fmap_df is not None:
-#         print("\nData transformation successful.")
-#         print("Cereal DF shape:", cereal_df.shape)
-#         print("FMAP DF shape:", fmap_df.shape)
-
-#         print("\nCereal sample:")
-#         print(cereal_df.head())
-
-#         print("\nFMAP sample:")
-#         print(fmap_df.head())
-
-#     else:
-#         print("Data transformation failed.")
-    
-#     load(cereal_df, fmap_df)
-#     if cereal_df is not None and fmap_df is not None:
-#         print("\nData loading successful.")
-#     else:
-#         print("Data loading failed.")
-#         print("\nPipeline complete.")
-
-#     print("Running cereal clustering model...\n")
-#     model_1()
-#     if model_1 is not None:
-#         print("\nCereal clustering model completed successfully.")
-#     else:
-#         print("\nCereal clustering model failed.")
-
-#     print("Running purchase trend model...\n")
-#     model_2()
-#     if model_2 is not None:
-#         print("\nPurchase trend model completed successfully.")
-#     else:
-#         print("\nPurchase trend model failed.")
-
-#     print("Running visualizations...\n")
-#     visualize_clusters()
-#     if visualize_clusters is not None:
-#         print("\nVisualizations completed successfully.")
-#     else:        
-#         print("\nVisualizations failed.")
 
     try:
         logging.info("Starting pipeline")
